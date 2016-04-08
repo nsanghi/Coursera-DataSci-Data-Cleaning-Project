84570cd56a1707fd676b62616e3b528d0ae0f69c
Nimish Sanghi <nimish.sanghi@gmail.com>
2016-04-08 14:34:54 +0530

    script finalised

---
+++ run_analysis.R
@@ -0,0 +1,99 @@
+###############################################################################
+#
+# Coursera Specialization - DataScience by John Hopkins
+# Data Cleaning Course Project
+#
+# run_analysis.R
+# This script will perform the steps as required in the coruse project.
+# The data is from URL 
+# https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+# 
+# The steps are 
+# 1. Merges the training and the test sets to create one data set.
+# 2. Extracts only the measurements on the mean and standard deviation 
+#    for each measurement.
+# 3. Uses descriptive activity names to name the activities in the data set.
+# 4. Appropriately labels the data set with descriptive variable names.
+# 5. From the data set in step 4, creates a second, independent tidy data set 
+#    with the average of each variable for each activity and each subject.
+# 
+# NOTE: The steps will not be performed in the strict order as enumerated above
+#
+# The data from URL is downloaded and kept in a directory named "dataset" which
+# is sibling to the directory from where this script is run
+###############################################################################
+
+# read train and test sets
+# read features X_train and X_test
+features_train <- read.table("../dataset/train/X_train.txt", header = FALSE)
+features_test <- read.table("../dataset/test/X_test.txt", header = FALSE)
+
+# read labels y_train and y_test
+labels_train <- read.table("../dataset/train/y_train.txt", header = FALSE)
+labels_test <-  read.table("../dataset/test/y_test.txt", header = FALSE)
+
+# read subject ids subject_train and subject_test
+subjects_train <- read.table("../dataset/train/subject_train.txt", header = FALSE)
+subjects_test <- read.table("../dataset/test/subject_test.txt", header = FALSE)
+
+
+# Step 4- name the variables appropriately
+
+# first load the names of the features
+feature_names <- read.table("../dataset/features.txt", header = FALSE)
+
+# set the names of features
+colnames(features_train) <- feature_names[,2] # set the name of the features
+colnames(features_test) <- feature_names[,2] # set the name of the features
+
+# set the label field name
+colnames(labels_train) <- "activity_id"
+colnames(labels_test) <- "activity_id"
+
+# set the subject field name
+colnames(subjects_train) <- "subject_id"
+colnames(subjects_test) <- "subject_id"
+
+# merge train data from three sources into a single dataframe
+train_data <- cbind(features_train,subjects_train, labels_train)
+test_data <- cbind(features_test,subjects_test, labels_test)
+
+# Step 1 - Merges the training and the test sets to create one data set.
+all_data <- rbind(train_data, test_data)
+
+# Step 2 - Extracts only the measurements on the mean and 
+#          standard deviation for each measurement.
+column_names <- colnames(all_data)
+
+selected_cols <- column_names[grepl("mean\\(\\)", column_names) 
+                              | grepl("std\\(\\)", column_names) 
+                              | grepl("subject_id", column_names)
+                              | grepl("activity_id", column_names)]
+
+final_data <- all_data[ ,selected_cols]
+
+# Step 3 - Uses descriptive activity names to name the activities in the data set
+# read description activity name
+activity_labels <- read.table("../dataset/activity_labels.txt", header = FALSE)
+colnames(activity_labels) <- c("activity_id", "activity_type")
+
+# merge final_data with activity_labels
+final_data <- merge(activity_labels, final_data, by="activity_id")
+
+# Step 5 From the data set in step 4, creates a second, independent tidy data 
+#set with the average of each variable for each activity and each subject.
+library(dplyr)
+
+# select data without the activity type (i.e. activity descriptions)
+data_step5 <- select(final_data, -activity_type)
+#group data by activity_id and subject_id
+grouped_data <- group_by(data_step5, activity_id, subject_id)
+#find mean for each column of the grouped data
+mean_grouped_data <- summarise_each(grouped_data, funs(mean))
+# add back activity_type
+mean_grouped_data <- merge(activity_labels, mean_grouped_data, by="activity_id")
+
+# Export the tidyData set 
+write.table(mean_grouped_data, './tidyData.txt',row.names=FALSE,sep='\t');
+
+
